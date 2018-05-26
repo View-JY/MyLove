384867ba21f7c315d7bf42586df1f520dc675fef
王亚雄 <946980051@qq.com>
2018-05-26 13:29:27 +0800

    新建了一个Users控制器,新建user/index文件作为用户列表页,数据可以遍历

---
+++ resources/views/users/index.blade.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<!--[if IE 6]><html class="ie lt-ie8"><![endif]-->
+<!--[if IE 7]><html class="ie lt-ie8"><![endif]-->
+<!--[if IE 8]><html class="ie ie8"><![endif]-->
+<!--[if IE 9]><html class="ie ie9"><![endif]-->
+<!--[if !IE]><!-->
+<html>
+ <!--<![endif]-->
+ <head> 
+  <meta charset="utf-8" /> 
+  <meta http-equiv="X-UA-Compatible" content="IE=Edge" /> 
+  <meta name="viewport" content="width=device-width, initial-scale=1.0,user-scalable=no" /> 
+  <!-- Start of Baidu Transcode --> 
+  <meta http-equiv="Cache-Control" content="no-siteapp" /> 
+  <meta http-equiv="Cache-Control" content="no-transform" /> 
+  <meta name="applicable-device" content="pc,mobile" /> 
+  <meta name="MobileOptimized" content="width" /> 
+  <meta name="HandheldFriendly" content="true" /> 
+  <meta name="mobile-agent" content="format=html5;url=https://www.jianshu.com/recommendations/users?utm_source=desktop&amp;utm_medium=index-users" /> 
+  <!-- End of Baidu Transcode --> 
+  <meta name="description" content="在简书，仍有数百万创作者在坚持产出优质创作，有数千万读者在用心交流创作；众多精彩创作，只在简书看得到" /> 
+  <meta name="keywords" content="推荐作者,签约作者,创作者,彭小六,剽悍一只猫,饱醉豚,纯银,安梳颜,一元亦有用,入江之鲸,韩大爷的杂货铺,尹沽城,林夏萨摩" /> 
+  <meta name="360-site-verification" content="604a14b53c6b871206001285921e81d8" /> 
+  <meta property="wb:webmaster" content="294ec9de89e7fadb" /> 
+  <meta property="qc:admins" content="104102651453316562112116375" /> 
+  <meta property="qc:admins" content="11635613706305617" /> 
+  <meta property="qc:admins" content="1163561616621163056375" /> 
+  <meta name="google-site-verification" content="cV4-qkUJZR6gmFeajx_UyPe47GW9vY6cnCrYtCHYNh4" /> 
+  <meta name="google-site-verification" content="HF7lfF8YEGs1qtCE-kPml8Z469e2RHhGajy6JPVy5XI" /> 
+  <meta http-equiv="mobile-agent" content="format=html5; url=https://www.jianshu.com/recommendations/users?utm_source=desktop&amp;utm_medium=index-users" /> 
+  <!-- Apple --> 
+  <meta name="apple-mobile-web-app-title" content="简书" /> 
+  <title>推荐作者 - 简书</title> 
+  <meta name="csrf-param" content="authenticity_token" /> 
+  <meta name="csrf-token" content="repcqo3Cf1q1WMgL/J3OlUiTs/xS5jHqn9SLhZAdZimxOvpIYoDvkmxKjoK5Hn43LlFIUjH++agkRwhdKWbxQw==" /> 
+  <link rel="stylesheet" media="all" href="css/web-454c23dafcf6c3369b7c.css" /> 
+  <link rel="stylesheet" media="all" href="css/entry-d61d60cb91db6dd7e79a.css" /> 
+  <link href="//cdn2.jianshu.io/assets/favicons/favicon-e743bfb1821442341c3ab15bdbe804f7ad97676bd07a770ccc9483473aa76f06.ico" rel="shortcut icon" type="image/x-icon" /> 
+  <link rel="apple-touch-icon-precomposed" href="//cdn2.jianshu.io/assets/apple-touch-icons/57-a6f1f1ee62ace44f6dc2f6a08575abd3c3b163288881c78dd8d75247682a4b27.png" sizes="57x57" /> 
+  <link rel="apple-touch-icon-precomposed" href="//cdn2.jianshu.io/assets/apple-touch-icons/72-fb9834bcfce738fd7b9c5e31363e79443e09a81a8e931170b58bc815387c1562.png" sizes="72x72" /> 
+  <link rel="apple-touch-icon-precomposed" href="//cdn2.jianshu.io/assets/apple-touch-icons/76-49d88e539ff2489475d603994988d871219141ecaa0b1a7a9a1914f4fe3182d6.png" sizes="76x76" /> 
+  <link rel="apple-touch-icon-precomposed" href="//cdn2.jianshu.io/assets/apple-touch-icons/114-24252fe693524ed3a9d0905e49bff3cbd0228f25a320aa09053c2ebb4955de97.png" sizes="114x114" /> 
+  <link rel="apple-touch-icon-precomposed" href="//cdn2.jianshu.io/assets/apple-touch-icons/120-1bb7371f5e87f93ce780a5f1a05ff1b176828ee0d1d130e768575918a2e05834.png" sizes="120x120" /> 
+  <link rel="apple-touch-icon-precomposed" href="//cdn2.jianshu.io/assets/apple-touch-icons/152-bf209460fc1c17bfd3e2b84c8e758bc11ca3e570fd411c3bbd84149b97453b99.png" sizes="152x152" /> 
+ </head> 
+ <body lang="zh-CN" class="reader-black-font"> 
+   
+  <div class="container recommend"> 
+   <img class="recommend-banner" src="picture/recommend-author-03cc8798d5cc3f986e49cbcb2eb63079.png" alt="Recommend author" /> 
+   <a target="_blank" class="help" href="/p/6df6dc693ab4"> <i class="iconfont ic-navigation-help"></i> 如何成为签约作者 </a> 
+   <div class="row"> 
+    <div class="col-xs-8"> 
+     <div class="wrap"> 
+      @foreach($data as $v)
+      <a target="_blank" href="/users/08e6960f7ed9"> <img class="avatar" src="picture/ed2ea7f3-9d7d-41fc-a05a-daebb8097b0c.jpg" alt="180" /> <h4 class="name">{{$v->name}}</h4> <p class="description">男，某军校新闻系毕业，曾任空某部指导员、机关...</p> </a> 
+      <div class="follow-button" props-data-user-id="11557689" props-data-following="false"></div> 
+      <hr /> 
+      <div class="meta">
+       最近更新
+      </div> 
+      <div class="recent-update"> 
+       <a class="new" target="_blank" href="/p/1798ef57e4ce">【军旅】异兵突起（24）破戒</a> 
+       <a class="new" target="_blank" href="/p/4536c639078b">【军旅】异兵突起（23）对弈</a> 
+       <a class="new" target="_blank" href="/p/6a51afab4063">【军旅】异兵突起（22）工地</a> 
+      </div> 
+     </div> 
+    </div> 
+   </div> 
+   @endforeach
+   <a class="btn btn-danger load-more-btn" data-next-page="2" href="">加载更多</a> 
+  </div>    
+ </body>
+</html>
