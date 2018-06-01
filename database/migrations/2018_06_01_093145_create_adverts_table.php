7ec01ddb922a9d2d5c3cabee7506976ad8c22367
View-JY <1154930416@qq.com>
2018-06-01 09:35:56 +0800

    新增广告位表

---
+++ database/migrations/2018_06_01_093145_create_adverts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAdvertsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('adverts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name')->index()->connect('广告名称');
+            $table->string('url')->connect('广告链接');
+            $table->string('image')->connect('广告图片');
+            $table->string('synopsis')->connect('广告简介');
+            $table->integer('weight')->connect('广告权重');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('adverts');
+    }
+}
